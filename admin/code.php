84d41437f07141f41d99e4ec038c75661e566315
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-04 15:31:18 +0700

    Pengaturan typo
    Tulisanne kurang ?> (fungsi php)

--- admin/code.php
+++ admin/code.php
@@ -10,21 +10,21 @@ if(isset($_POST['registerbtn']))
     $cpassword = $_POST['confirmpassword'];
     //data yang akan dikirim ke database berupa username, email, password
     //cpassword digunakan untuk melakukan pengulangan password
-        $query = "INSERT INTO register (username,email,password) VALUES ('$username','$email','$password')";
-        $query_run = mysqli_query($connection, $querry);
-
         
     if($password === $cpassword)
     {  
+        $query = "INSERT INTO register (username,email,password) VALUES ('$username','$email','$password')";
+        $query_run = mysqli_query($connection, $querry);
+
         if($query_run)
         {
             //echo "Disimpan";
-            $_SESSION['success'] = "Profil berhasil disimpan";
+            $_SESSION['success'] = "Admin Profile Added";
             header('Location: register.php');
         }
         else
         {
-            $_SESSION['status'] = "Profil gagal disimpan";
+            $_SESSION['status'] = "Admin Profile not Added";
             header('Location: register.php');
         }
     }
@@ -34,3 +34,5 @@ if(isset($_POST['registerbtn']))
         header('location: register.php');
     }
 }
+
+?>
