ad4a6d8d4f07af2a0a638b2d3b98c16756a6256d
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-06 21:05:49 +0700

    Text box untuk edit pengguna

--- admin/register_edit.php
+++ admin/register_edit.php
@@ -8,12 +8,29 @@ include('includes/navbar.php');
 
 <!--Bagian atas -->>
 <div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary"> Ubah Pengguna 
-    </h6>
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Ubah Pengguna</h6>
+    </div>
+<div class="card-body">
+<!---Akhir bagian atas---->
 
-  </div>
-</div class="card-body">
+<!---Mulai untuk form---->
+    <div class="form-group">
+        <label> Nama Pengguna </label>
+        <input type="text" name="username" class="form-control" placeholder="Enter Username">
+    </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="email" class="form-control" placeholder="Enter Email">
+        </div>
+        <div class="form-group">
+            <label>Kata Sandi</label>
+            <input type="password" name="password" class="form-control" placeholder="Enter Password">
+        </div>
+
+</div>
+</div>
+</div>
 
 <?php
 include('includes/scripts.php');
