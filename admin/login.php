990f37923521976d5364f6b6082a76ae1a891658
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-21 22:52:59 +0700

    translet tipis

--- admin/login.php
+++ admin/login.php
@@ -20,27 +20,26 @@ include('includes/header.php');
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang</h1>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                                placeholder="Masukkan Email Anda">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                                id="exampleInputPassword" placeholder="Masukkan Password Anda">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
+                                                <label class="custom-control-label" for="customCheck">Ingat Saya</label>
                                             </div>
                                         </div>
                                         <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
+                                            Masuk
                                         </a>
                                 </div>
                             </div>
