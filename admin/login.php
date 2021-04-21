d68ba5c5090d0fa0352fe178d4c6ba64a2ea8e0b
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-22 05:41:49 +0700

    edit pembatas

--- admin/login.php
+++ admin/login.php
@@ -16,15 +16,12 @@ include('includes/header.php');
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Selamat Datang</h1>
                                     </div>
                                     <form class="user" action="code.php" method="post">
-
-
                                     <form class="user">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user"
@@ -34,6 +31,7 @@ include('includes/header.php');
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
                                                 id="exampleInputPassword" placeholder="Masukkan Password Anda">
+                                        </div>
                                         <a href="index.html" class="btn btn-primary btn-user btn-block">
                                             Masuk
                                         </a>
