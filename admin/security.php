cbe02b6712b826f7db964ea5e8736ba177f3799d
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-25 23:15:17 +0700

    Cukur kode security

--- admin/security.php
+++ admin/security.php
@@ -1,16 +1,5 @@
 <?php
 session_start();
-include('database/dbconfig.php');
-
-if($connection)
-{
-    //Echo "Database tersambung"
-}
-else
-{
-    header("Location: database/dbconfig.php");
-}
- 
 //berfungsi untuk pengaman, biar gak bisa diakses direct ke index pp
 //tanpa login dahulu
 if(!$_SESSION['username'])
