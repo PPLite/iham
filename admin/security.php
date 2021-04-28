6722fe5eddffe0feb80b9f56c68f7f68f1efb135
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-28 23:02:18 +0700

    tes2

--- admin/security.php
+++ admin/security.php
@@ -2,9 +2,7 @@
 session_start();
 //berfungsi untuk pengaman, biar gak bisa diakses direct ke index php dan halaman lainnya yang sudah di masukin secirity php
 //tanpa login dahulu
-$_SESSION['usertype'];
-
-if(!$_SESSION['username'])
+if(!$_SESSION['username']['usertype'])
 {
     header('Location: login.php');
 }
