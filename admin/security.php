c61f62481b2773b3df52d78300c4c55fc80a23ec
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-28 22:59:15 +0700

    menghancurkan kodingan

--- admin/security.php
+++ admin/security.php
@@ -2,6 +2,8 @@
 session_start();
 //berfungsi untuk pengaman, biar gak bisa diakses direct ke index php dan halaman lainnya yang sudah di masukin secirity php
 //tanpa login dahulu
+$_SESSION['usertype'];
+
 if(!$_SESSION['username'])
 {
     header('Location: login.php');
