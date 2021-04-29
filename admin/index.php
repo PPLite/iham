5c505550292effc369645d60e2774c9edf5ebd4c
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-30 02:05:18 +0700

    tes obrak abrik index

--- admin/index.php
+++ admin/index.php
@@ -39,7 +39,6 @@ include('database/dbconfig.php');
               </div>
             </div>
             <div class="col-auto">
-             <i class="far fa-address-card-2x -300"></i>
             </div>
           </div>
         </div>
@@ -52,11 +51,10 @@ include('database/dbconfig.php');
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Earnings (Annual)</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Asset Terdaftar</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">30 Asset</div>
             </div>
             <div class="col-auto">
-              <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
             </div>
           </div>
         </div>
@@ -69,21 +67,13 @@ include('database/dbconfig.php');
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks</div>
-              <div class="row no-gutters align-items-center">
-                <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                </div>
-                <div class="col">
-                  <div class="progress progress-sm mr-2">
-                    <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50"
-                      aria-valuemin="0" aria-valuemax="100"></div>
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Asset Terpakai</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">21 Asset</div>
                   </div>
                 </div>
               </div>
             </div>
             <div class="col-auto">
-              <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
             </div>
           </div>
         </div>
