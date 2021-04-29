3fad8dbf2f535d8eb3f2e9028df034a90f166976
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-30 01:19:58 +0700

    dibalekne

--- admin/security.php
+++ admin/security.php
@@ -2,7 +2,7 @@
 session_start();
 //berfungsi untuk pengaman, biar gak bisa diakses direct ke index php dan halaman lainnya yang sudah di masukin secirity php
 //tanpa login dahulu
-if(!$_SESSION['usertype'] && ['username'])
+if(!$_SESSION['username'])
 {
     header('Location: login.php');
 }
