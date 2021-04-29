e261f8c794c79af7744f07cf1fa5463fc124596e
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-30 02:12:21 +0700

    morat marit part 1

--- admin/index.php
+++ admin/index.php
@@ -60,36 +60,6 @@ include('database/dbconfig.php');
         </div>
       </div>
     </div>
-
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-info shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Asset Terpakai</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">21 Asset</div>
-            <div class="col-auto">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Pending Requests Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-warning shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Aset Dipesan</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
-            <div class="col-auto">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 
   <!-- Content Row -->
