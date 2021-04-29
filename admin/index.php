1c34857d556739a66997b6d5f62435ff77309afa
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-04-30 02:24:34 +0700

    tes10

--- admin/index.php
+++ admin/index.php
@@ -47,7 +47,7 @@ include('database/dbconfig.php');
 
     <!-- Kartu 2 -->
     <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-info shadow h-100 py-2">
+      <div class="card border-left-success shadow h-100 py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -63,7 +63,7 @@ include('database/dbconfig.php');
 
     <!-- Kartu 3 -->
     <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-warning shadow h-100 py-2">
+      <div class="card border-left-info shadow h-100 py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -79,7 +79,7 @@ include('database/dbconfig.php');
 
     <!-- Kartu 4 -->
     <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-success shadow h-100 py-2">
+      <div class="card border-left-warning shadow h-100 py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
