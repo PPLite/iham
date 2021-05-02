f85c3f9096b7b4b5b1adbd1d1252071d3ff64360
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-03 01:11:59 +0700

    perbaiki bug

--- admin/includes/security-admin.php
+++ admin/includes/security-admin.php
@@ -1,8 +1,7 @@
 <?php
-session_start();
 //berfungsi untuk pengaman, biar gak bisa diakses direct ke index php dan halaman lainnya yang sudah di masukin secirity php
 //tanpa login dahulu
-if(!$_SESSION['usertype'] == admin)
+if(!$_SESSION['usertype'] == "admin")
 {
     header('Location: index.php');
 }
