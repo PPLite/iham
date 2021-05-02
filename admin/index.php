0f43d2618fdedf56b72511c2111d5c138ba557e9
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-02 22:53:43 +0700

    tes hyperlink

--- admin/index.php
+++ admin/index.php
@@ -21,12 +21,12 @@ include_once('database/dbconfig.php');
   <div class="row">
 
     <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4" href="register.php">
+    <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-primary shadow h-100 py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Pengguna Terdaftar</div>
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1" href="register.php">Jumlah Pengguna Terdaftar</div>
               <div class="h5 mb-0 font-weight-bold text-gray-800">
               <?php
               $query = "SELECT id FROM register ORDER BY id";
