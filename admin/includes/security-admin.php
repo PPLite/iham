cf94f514c543ab197ebcfb9b6d7a57afdfb31a09
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-03 01:16:27 +0700

    bug terakhir. mulai lelah

--- admin/includes/security-admin.php
+++ admin/includes/security-admin.php
@@ -1,7 +1,11 @@
 <?php
 //berfungsi untuk pengaman, biar gak bisa diakses direct ke index php dan halaman lainnya yang sudah di masukin secirity php
 //tanpa login dahulu
-if(!$_SESSION['usertype'] == "admin")
+if($_SESSION['usertype'] == "admin")
+{
+    header();
+}
+else
 {
     header('Location: index.php');
 }
