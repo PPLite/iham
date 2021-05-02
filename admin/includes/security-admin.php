a0b420dbf20f6e3c277008d3a516db0debdb8190
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-03 01:10:16 +0700

    testing class

---
+++ admin/includes/security-admin.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+//berfungsi untuk pengaman, biar gak bisa diakses direct ke index php dan halaman lainnya yang sudah di masukin secirity php
+//tanpa login dahulu
+if(!$_SESSION['usertype'] == admin)
+{
+    header('Location: index.php');
+}
