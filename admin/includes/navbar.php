84591af732b9b397f29e881abf941c12971b31c3
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-02 22:38:22 +0700

    nengahne

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -290,7 +290,6 @@ include_once('code.php');
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
 
-              <!--- Untuk menampilkan pengguna yang sedang aktif saat itu--->
               <?php
                echo $_SESSION['usertype']; 
               
