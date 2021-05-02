e3e04e2999ff19e9e9ee69da43c283446056dcc5
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-02 22:39:46 +0700

    ngilak'i seng gak penting

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -287,36 +287,14 @@ include_once('code.php');
             <div class="topbar-divider d-none d-sm-block"></div>
 <!-------- Pembatas----------------->
             <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">
 
               <?php
                echo $_SESSION['usertype']; 
-              
               ?> 
               
                 </span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
-              </a>
-              <!-- Dropdown - User Information -->
-              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profil
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Pengaturan
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Log Aktivitas
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Keluar
-                </a>
               </div>
             </li>
 <!-------- Pembatas----------------->
