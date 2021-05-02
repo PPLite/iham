822f82953ff3aca2f61a66f3fd6ae2796425beb7
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-03 00:25:23 +0700

    cobak sampek isok

--- admin/register.php
+++ admin/register.php
@@ -145,21 +145,23 @@ include('database/dbconfig.php')
 <!----Menu buat hapus data pengguna--->
             <td>
 
-
-
-
-
+            <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">
+                        Hapus 
+                      </button>
+            </div>
 
             <div class="modal fade" id="addadminprofile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Tambahkan data Pengguna</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form action="code.php" method="POST">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Tambahkan data Pengguna</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+        <form action="code.php" method="POST">
 
         <div class="modal-body">
 
