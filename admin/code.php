b579efd56155949661a09824be0636250fbfe446
“PPlite” <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-09 22:36:39 +0700

    mundur dulu

--- admin/code.php
+++ admin/code.php
@@ -189,7 +189,7 @@ if(isset($_POST['daftaraset_btn']))
 //    }
 //    else
 //    {
-        $query = "INSERT INTO tb_rfid ('nama_alat','uid','deskripsi','penanggung_jawab','status_asset','gambar_asset') VALUES ('$nama_alat','$uid','$deskripsi','$penanggung_jawab','$status_asset','$gambar_asset')";
+        $query = "INSERT INTO tb_rfid ('nama_alat','uid','deskripsi','penanggung_jawab','status_asset') VALUES ('$nama_alat','$uid','$deskripsi','$penanggung_jawab','$status_asset',)";
         $query_run = mysqli_query($connection, $query);
 
         if($query_run)
