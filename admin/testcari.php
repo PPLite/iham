6b1d4dfc100c954ec572c817154ee637ed30b391
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-17 21:51:30 +0700

    tes cari asset / tambah aset

---
+++ admin/testcari.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>AJAX Database</title>
+    </head>
+    <body>
+        Name : <input type="text" id="name">
+        <input type="submit" id="name-submit" value="Grab">
+        <div id="name-data"></div>
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="includes/ambildata.js"></script>
+    </body>
+</html>
