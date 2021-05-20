b00fa8f989e786178e43a6454fdd9d5dc9f152a4
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-05-20 13:39:02 +0700

    bersih-bersih

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -69,28 +69,17 @@ include_once('code.php');
     <span>Pengaturan Pengguna</span></a>
 </li>
 
-
-<li class="nav-item">
-  <a class="nav-link" href="baca-tag.php">
-    <i class="fas fa-fw fa-wrench"></i>
-    <span>Baca Tag</span></a>
-</li>
-
-
-
 <!-- Nav Item - Utilities Collapse Menu -->
 <li class="nav-item">
   <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
     <i class="fas fa-fw fa-wrench"></i>
-    <span>Utilities</span>
+    <span>Pengaturan RFID</span>
   </a>
   <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <h6 class="collapse-header">Custom Utilities:</h6>
-      <a class="collapse-item" href="utilities-color.html">Colors</a>
-      <a class="collapse-item" href="utilities-border.html">Borders</a>
-      <a class="collapse-item" href="utilities-animation.html">Animations</a>
-      <a class="collapse-item" href="utilities-other.html">Other</a>
+      <h6 class="collapse-header">Pengaturan RFID:</h6>
+      <a class="collapse-item" href="fitur-rfid/baca-tag.php">Baca RFID TAG</a>
+      <a class="collapse-item" href="fitur-rfid/status-asset.php">Status Asset</a>
     </div>
   </div>
 </li>
