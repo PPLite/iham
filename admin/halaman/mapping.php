5ea17eb8272f086173fff6aa72034e3cee33f7b3
ajisolicha <43943999+ajisolicha@users.noreply.github.com>
2021-06-03 12:54:57 +0700

    Add files via upload

--- admin/halaman/mapping.php
+++ admin/halaman/mapping.php
@@ -8,7 +8,7 @@ include('../database/dbconfig.php');
     //dari database, dipilih semua (bintang = semuanya) dari tabel "tb_rfid"
     $query = "SELECT rfid_uid,timestamp,nama_anak
               FROM tb_scanner1_assetbayi
-              ORDER BY `timestamp` DESC LIMIT 3
+              ORDER BY `timestamp` DESC LIMIT 5
     "; 
     $query_run = mysqli_query($connection, $query);
 	$row = mysqli_fetch_assoc($query_run);
@@ -19,7 +19,7 @@ include('../database/dbconfig.php');
     //dari database, dipilih semua (bintang = semuanya) dari tabel "tb_rfid"
        $query_2= "SELECT rfid_uid,timestamp, nama_anak
               FROM tb_scanner2_assetbayi
-              ORDER BY `timestamp` DESC LIMIT 3
+              ORDER BY `timestamp` DESC LIMIT 5
     "; 
     $query_run2 = mysqli_query($connection, $query_2);
 	$row_2 = mysqli_fetch_assoc($query_run2);
@@ -45,7 +45,7 @@ include('../database/dbconfig.php');
       <table class="table table-bordered" id="" width="100%" cellspacing="0">
         <thead>
           <tr>
-            <th>Timestamp</th>
+			<th>timestamp</th>
             <th>RFID UID</th>
             <th>Nama Anak</th>
           </tr>
@@ -60,15 +60,19 @@ include('../database/dbconfig.php');
           {
 			  ?>
 			<tr>
-			<?php
-			if($row['timestamp']>= $row_2['timestamp']){?>
-			<td><?php echo $row['timestamp']; ?></td> 
+			<td><?php echo $row['timestamp']; ?></td>
 			<td><?php echo $row['rfid_uid']; ?></td> 
 			<td><?php echo $row['nama_anak']; ?></td> <?php
-			}
-			else{
-				echo "Data tidak ditemukan";
-			}
+			
+			//if($row['rfid_uid'] ==  $last || $row['rfid_uid'] == 0 ){?>
+			<?php break;?>
+			<?php
+			//}
+			//else{?>
+				<?php //echo"Tidak ada pasien yang terdeteksi"; ?>
+			<?php
+			//}
+			//$last = $row['rfid_uid'];
 			?>
 			
 		<?php	
@@ -106,7 +110,7 @@ include('../database/dbconfig.php');
       <table class="table table-bordered" id="" width="100%" cellspacing="0">
         <thead>
           <tr>
-            <th>Timestamp</th>
+			<th>timestamp</th>
             <th>RFID UID</th>
             <th>Nama Anak</th>
           </tr>
@@ -115,26 +119,25 @@ include('../database/dbconfig.php');
         <?php
         //mengambil data dari database
         //tipe kolom yang nantinya akan diambil
-        if(mysqli_num_rows($query_run) > 0 )
+        if(mysqli_num_rows($query_run2) > 0 )
         {
           while($row_2 = mysqli_fetch_assoc($query_run2))
           {
 			  ?>
 			<tr>
 			<?php
-			if ($row_2['timestamp'] >= $row['timestamp']){
-			?>
-			<td><?php echo $row_2['timestamp']; ?></td> 
+			//if($row_2['timestamp'] >=  $row['timestamp']){?>
+			<td><?php echo $row_2['timestamp']; ?></td>
 			<td><?php echo $row_2['rfid_uid']; ?></td> 
-			<td><?php echo $row_2['nama_anak']; ?></td><?php
-			
-			}
-			else{
-				echo "Data tidak ditemukan";
-			}?> 
+			<td><?php echo $row_2['nama_anak']; ?></td> <?php
+			//}
+			//else{?>
+				<?php //echo"Tidak ada pasien yang terdeteksi"; ?>
+			<?php
+			//}
+			?>
 		<?php	
 		  }
-
 		}
       
         //Jika gagal ngambil data akan mengeluarkan peringatan
@@ -150,146 +153,6 @@ include('../database/dbconfig.php');
 </div>
 </div>
 
-<div class="container-fluid">
-
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary"> Hasil Scanning RFID Reader 2 (Aset Barang)
-    </h6>
-  </div>
-
-<div class="card-body">
-
-    <div class="table-responsive">
-
-
-<!---Buat ngambil data--->
-<?php
-    //dari database, dipilih semua (bintang = semuanya) dari tabel "tb_rfid"    
-    $query = "SELECT * FROM `tb_hasilscan2_asetbarang` ORDER BY `tb_hasilscan2_asetbarang`.`timestamp` DESC";
-    $query_run = mysqli_query($connection, $query);
-    ?>
-
-      <table class="table table-bordered" id="" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-            <th>Timestamp</th>
-            <th>RFID UID</th>
-            <th>Nama Alat</th>
-            <th>Deskripsi</th>
-            <th>Penanggung Jawab</th>
-            <th>Status Aset</th>
-            <th>Peminjam</th>
-            <th>Jumlah Terbaca</th>
-          </tr>
-        </thead>
-        <tbody>
-        <?php
-        //mengambil data dari database
-        //tipe kolom yang nantinya akan diambil
-        if(mysqli_num_rows($query_run) > 0 )
-        {
-          while($row = mysqli_fetch_assoc($query_run))
-          {
-            ?>
-          <tr>
-          <!---Mengambil data dari database kemudian menampilkan ke tabel, serta menentukan kolom mana saja yang akan diambil datanya-->
-            <td><?php echo $row['timestamp']; ?></td>
-            <td><?php echo $row['rfid_uid']; ?></td>
-            <td><?php echo $row['nama_alat']; ?></td>
-            <td><?php echo $row['deskripsi']; ?></td>
-            <td><?php echo $row['penanggung_jawab']; ?></td>
-            <td><?php echo $row['status_asset']; ?></td>
-            <td><?php echo $row['peminjam']; ?></td>
-            <td><?php echo $row['jumlah']; ?>x</td>  
-
-          <?php
-          }
-        }
-        //Jika gagal ngambil data akan mengeluarkan peringatan
-        else {
-          echo "Data tidak ditemukan";   
-        }
-        ?>        
-        </tbody>
-      </table>
-
-    </div>
-  </div>
-</div>
-</div>
-
-<div class="container-fluid">
-
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary"> Hasil Scanning RFID Reader 2 (Aset Bayi)
-    </h6>
-  </div>
-
-
-
-<div class="card-body">
-
-    <div class="table-responsive">
-
-
-<!---Buat ngambil data--->
-<?php
-    //dari database, dipilih semua (bintang = semuanya) dari tabel "tb_rfid"
-    $query = "SELECT * FROM `tb_hasilscan2_asetbayi` ORDER BY `tb_hasilscan2_asetbayi`.`timestamp` DESC"; 
-    $query_run = mysqli_query($connection, $query);
-    ?>
-
-      <table class="table table-bordered" id="" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-            <th>Timestamp</th>
-            <th>RFID UID</th>
-            <th>ID Pengenal</th>
-            <th>Nama Anak</th>
-            <th>Nama Ibu</th>
-            <th>Penanggung Jawab Bayi</th>
-            <th>Alamat</th></th>
-            <th>Status</th>
-            <th>Jumlah Terbaca</th>
-          </tr>
-        </thead>
-        <tbody>
-        <?php
-        //mengambil data dari database
-        //tipe kolom yang nantinya akan diambil
-        if(mysqli_num_rows($query_run) > 0 )
-        {
-          while($row = mysqli_fetch_assoc($query_run))
-          {
-            ?>
-          <tr>
-          <!---Mengambil data dari database kemudian menampilkan ke tabel, serta menentukan kolom mana saja yang akan diambil datanya-->
-          <td><?php echo $row['timestamp']; ?></td>
-            <td><?php echo $row['rfid_uid']; ?></td>
-            <td><?php echo $row['id_pengenal']; ?></td>
-            <td><?php echo $row['nama_anak']; ?></td>
-            <td><?php echo $row['nama_ibu']; ?></td>
-            <td><?php echo $row['penanggung_jawab_bayi']; ?></td>
-            <td><?php echo $row['alamat']; ?></td>
-            <td><?php echo $row['status']; ?></td>
-            <td><?php echo $row['jumlah']; ?>x</td>
-          <?php
-          }
-        }
-        //Jika gagal ngambil data akan mengeluarkan peringatan
-        else {
-          echo "Data tidak ditemukan";   
-        }
-        ?>        
-        </tbody>
-      </table>
-
-    </div>
-  </div>
-</div>
-</div>
 
 <?php
 include('../includes/scripts.php');
