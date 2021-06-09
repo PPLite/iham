a17153d264923cffaa99f58e77fd9485a08e3f38
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-09 10:00:33 +0000

    bismillah isok regis logika

--- admin/code.php
+++ admin/code.php
@@ -340,6 +340,43 @@ if(isset($_POST['daftarasetanak_btn']))
     $penanggung_jawab_bayi = $_POST['penanggung_jawab_bayi'];
     $alamat = $_POST['alamat'];
     $status = $_POST['status'];
+    
+    //SELECT * FROM tb_stat_anak WHERE rfid_uid = 'e20000202716018204409d9f' AND status = 'checkout'
+    $cekanak_query = "SELECT * FROM tb_stat_anak 
+    WHERE rfid_uid= '$rfid_uid' OR status= 'checkout' ";
+    $cekanak_query_run = mysqli_query($connection, $cekanak_query);
+    if(mysqli_num_rows($cekanak_query_run) > 0)
+    {
+        $query = "INSERT INTO tb_stat_anak (rfid_uid,id_pengenal,nama_anak,nama_ibu,penanggung_jawab_bayi,alamat,status) VALUES ('$rfid_uid','$id_pengenal','$nama_anak','$nama_ibu','$penanggung_jawab_bayi','$alamat','$status')";
+        $query_run = mysqli_query($connection,$query);
+
+        if($query_run)
+        {
+            if($query_run)
+            {
+            $_SESSION['status'] ="Data Berhasil di Ubah";
+            $_SESSION['status_code'] = "success";
+            header('location: daftar-bayi.php');
+            }
+            else
+            {
+                $_SESSION['status'] ="Data Berhasil di Ubah";
+                $_SESSION['status_code'] = "error";
+                header('location: daftar-bayi.php');
+            }
+        }
+    }
+    else
+    {
+        $_SESSION['status'] ="Registrasi Gagal. Email/Username Telah Terdaftar";
+        $_SESSION['status_code'] = "error";
+        header('location: daftar-bayi.php');
+    }
+    ////////////
+    
+    
+    
+    
     $query = "INSERT INTO tb_stat_anak (rfid_uid,id_pengenal,nama_anak,nama_ibu,penanggung_jawab_bayi,alamat,status) VALUES ('$rfid_uid','$id_pengenal','$nama_anak','$nama_ibu','$penanggung_jawab_bayi','$alamat','$status')";
     $query_run = mysqli_query($connection,$query);
 
