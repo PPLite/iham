dad2587ebeb661bb028c4a772a2a6ad3284775f4
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-10 15:07:56 +0000

    oto

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -30,20 +30,20 @@ include('database/dbconfig.php');
         </div>
 
         <div class="row">
-                    <div class="col-lg-5 mb-4">
+                    <div class="col-lg-5 mb-4" href="tambah-aset-barang.php">
                       <div class="card bg-primary text-white shadow">
-                        <div class="card-body" href="tambah-aset-barang.php">
-                            <a href="tambah-aset-barang.php">Registrasi Barang</a>
+                        <div class="card-body">
+                            Registrasi Barang
                         <div class="float-right">
                           <i class="fas fa-archive fa-2x text-gray-300"></i>
                         </div>
                       </div>
                     </div>
                   </div>
-                <div class="col-lg-5 mb-4">
+                <div class="col-lg-5 mb-4" href="tambah-aset-bayi.php">
                     <div class="card bg-success text-white shadow">
-                  <div class="card-body">
-                  <a href="tambah-aset-bayi.php">Registrasi bayi</a>
+                  <div class="card-body" >
+                            Registrasi bayi
                   <div class="float-right">
                 <i class="fas fa-baby-carriage fa-2x text-gray-300"></i>
                 </div>
