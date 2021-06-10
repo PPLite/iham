a988b6feb36e94bb38aaecb6ab8b060ba1533b90
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-10 04:29:43 +0000

    tes9

--- admin/index.php
+++ admin/index.php
@@ -155,19 +155,25 @@ include_once('database/dbconfig.php');
                                 <div class="card-body">
                                     <div class="chart-area">
                                         
-                                        <div class="card mb-4 py-3 border-bottom-success">
-                                          <div class="card-body">
-                                            .border-bottom-success
-                                          </div>
-                                        </div>
 
-                                        <div class="card mb-4 py-3 border-bottom-info">
-                                        .border-bottom-info.border-bottom-info
-                                          <div class="card-body">
-                                            .border-bottom-success
-                                          </div>
-                                        </div>
 
+                                        
+                                    <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Earnings (Annual)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                                     </div>
                                 </div>
