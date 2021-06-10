1c3aecd791696df67ec3dd173fbefaa6e81ac204
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-10 11:33:05 +0000

    mbuh wes

--- admin/index.php
+++ admin/index.php
@@ -127,10 +127,11 @@ include_once('database/dbconfig.php');
 
 
         <div class="row">
-          <div class="col-lg-5 mb-4" id="accordion">
+          <div class="col-lg-5 mb-4">
             <div class="card shadow mb-4">
               <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary" data-toggle="collapse" data-parent="#accordion" href="#collapse1">Registrasi</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Registrasi</h6>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
               </div>
                 <div class="card-body">
                   <div class="row">
