adf7e7bb9c71981a862f3bfe71279266c12dcfb5
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-10 04:20:54 +0000

    tes4

--- admin/index.php
+++ admin/index.php
@@ -130,6 +130,7 @@ include_once('database/dbconfig.php');
                         <!-- Area Chart -->
                         <div class="col-xl-8 col-lg-7">
                             <div class="card shadow mb-4">
+
                                 <!-- Card Header - Dropdown -->
                                 <div
                                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -149,10 +150,19 @@ include_once('database/dbconfig.php');
                                         </div>
                                     </div>
                                 </div>
+
                                 <!-- Card Body -->
                                 <div class="card-body">
                                     <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
+                                        
+                                    <div class="card mb-4 py-3 border-bottom-success">
+                                      <div class="card-body">
+                                        .border-bottom-success
+                                      </div>
+                                </div>
+
+
+
                                     </div>
                                 </div>
                             </div>
