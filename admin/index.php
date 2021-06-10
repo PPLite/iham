2214fb6e29a9640c47ff3face8a1daab3a863392
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-10 11:18:22 +0000

    tes

--- admin/index.php
+++ admin/index.php
@@ -128,8 +128,8 @@ include_once('database/dbconfig.php');
 
         <div class="row">
           <div class="col-lg-5 mb-4">
-            <div class="card shadow mb-4" id="accordion">
-              <div class="card-header py-3">
+            <div class="card shadow mb-4">
+              <div class="card-header py-3" id="accordion">
                 <h6 class="m-0 font-weight-bold text-primary">Registrasi</h6>
               </div>
                 <div class="card-body">
