bca38855efe9333ea140c3efaa75bc097195e2e1
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-10 11:10:02 +0000

    cekuaks

--- admin/index.php
+++ admin/index.php
@@ -12,9 +12,9 @@ include_once('database/dbconfig.php');
 
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    <h1 class="h3 mb-0 text-gray-800">IHAM Dashboard </h1>
     <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-        class="fas fa-download fa-sm text-white-50"></i> Buat Laporan</a>
+        class="fas fa-download fa-sm text-white-50"></i> Unduh Laporan Hari Ini</a>
   </div>
 
   <!-- Content Row -->
@@ -126,41 +126,37 @@ include_once('database/dbconfig.php');
   </div>
 
 
-<!-- Content Row -->
-<div class="row">
-  <!-- Content Column -->
-  <div class="col-lg-5 mb-4">
 
-    <!-- Project Card Example -->
-    <div class="card shadow mb-4">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Registrasi</h6>
-      </div>
-      <div class="card-body">
-
-      <div class="row">
-                                <div class="col-lg-5 mb-4">
-                                    <div class="card bg-primary text-white shadow">
-                                        <div class="card-body">
-                                            Registrasi Barang
-                                            <div class="float-right">
-                                            <i class="fas fa-archive fa-2x text-gray-300"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-
-                                <div class="col-lg-5 mb-4">
-                                    <div class="card bg-success text-white shadow">
-                                        <div class="card-body">
-                                            Registrasi Bayi
-                                            <div class="float-right">
-                                            <i class="fas fa-baby-carriage fa-2x text-gray-300"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+        <div class="row">
+          <div class="col-lg-5 mb-4">
+            <div class="card shadow mb-4">
+              <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Registrasi</h6>
+              </div>
+                <div id="accordion">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-lg-5 mb-4">
+                        <div class="card bg-primary text-white shadow">
+                          <div class="card-body">
+                          Registrasi Barang
+                          <div class="float-right">
+                            <i class="fas fa-archive fa-2x text-gray-300"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                      <div class="col-lg-5 mb-4">
+                      <div class="card bg-success text-white shadow">
+                    <div class="card-body">
+                      Registrasi Bayi
+                    <div class="float-right">
+                  <i class="fas fa-baby-carriage fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
 
