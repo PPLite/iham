238be9cfe9eb9ac90ea424e32de0205b8e60c031
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-10 15:29:28 +0000

    tes3

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -18,7 +18,7 @@ include('database/dbconfig.php');
                   <div class="row">
                     <div class="col-lg-5 mb-4">
                       <div class="card bg-primary text-white shadow">
-                        <div class="card-body" href="tambah-aset-barang.php">
+                        <div class="card-body" >
                         Registrasi Barang
                         <div class="float-right">
                           <i class="fas fa-archive fa-2x text-gray-300"></i>
@@ -27,6 +27,7 @@ include('database/dbconfig.php');
                     </div>
                   </div>
                     <div class="col-lg-5 mb-4">
+                    <a href="tambah-aset-bayi.php">
                     <div class="card bg-success text-white shadow">
                   <div class="card-body">
                     Registrasi Bayi
@@ -39,7 +40,6 @@ include('database/dbconfig.php');
         </div>
       </div>
     </div>
-</div>
 <!--- Yang dulunya pernah jadi dropdown
 <div class="bs-example">
     <div class="dropdown">
