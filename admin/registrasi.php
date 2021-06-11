41ced5b481bc531c10da208f70278c58cfc85586
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-11 17:28:44 +0000

    kunsstulll

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -10,25 +10,20 @@ include('database/dbconfig.php');
         <div class="col-md-7">
 
           <div class="card mt-5">
+
                 <div class="card-header text-center">
                     <h4> Registrasi Asset Barang Baru</h4>
                 </div>
 
-                    <div class="row"> <!-- Pembuka buat "ROW" / kotak werno werni -->
-                        <div class="row justify-content-center">
-                                <div class="col-lg-5 mb-4">
-                                    <div class="card bg-primary text-white shadow">
-                                        <div class="card-body">
-                                            Primary
-                                            <div class="text-white-50 small">#4e73df</div>
-                                            <i class="fas fa-archive fa-2x text-gray-300 float-right"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                         </row>
-                    </div> <!-- Penutup buat "ROW" -->        
-                                
-            </div>
+                <div class="card-body">
+                    <div class="row justify-content-center">
+                    Hewes Hewes nang tengah
+                    </div>
+                </div>
+
+
+
+          </div>
             
         </div>   
     </div>
