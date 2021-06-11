37d7278fbb4727b773572b8a6d8c1442ed5e69ae
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-11 17:07:05 +0000

    satu kotak

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -13,11 +13,17 @@ include('database/dbconfig.php');
                 <div class="card-header text-center">
                     <h4> Registrasi Asset Barang Baru</h4>
                 </div>
-                        <div class="card-body">
-                            <div class="col-md-12">
-                                <button type="SUBMIT" class="btn btn-primary">Scan RFID</button>
-                            </div>
-                        </div>
+
+                <div class="row">
+                                <div class="col-lg-6 mb-4">
+                                    <div class="card bg-primary text-white shadow">
+                                        <div class="card-body">
+                                            Primary
+                                            <div class="text-white-50 small">#4e73df</div>
+                                        </div>
+                                    </div>
+                                </div>
+                </div>
 
                         <div class="card-body">
                             <div class="col-md-12">
