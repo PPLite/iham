4a57a64cca39dca42a2c6c9e4048505875831944
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-11 15:27:33 +0000

    hmmm

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -13,29 +13,19 @@ include('database/dbconfig.php');
                 <div class="card-header text-center">
                     <h4> Registrasi Asset Barang Baru</h4>
                 </div>
-                      <div class="card-body">
-                        <div class="row">
-                          <div class="col-lg-5 mb-4">
-                          <a href="tambah-aset-barang.php">
-                            <div class="card bg-primary text-white shadow">
-                              <div class="card-body">
-                                Registrasi Barang
-                                    <div class="float-right">
-                                      <i class="fas fa-archive fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                              </div>
-                          </div> 
+                        <div class="card-body">
+                            <div class="col-md-12">
+                                <button type="SUBMIT" class="btn btn-primary">Scan RFID</button>
+                            </div>
                         </div>
-                      </div> 
-                        
 
                         <div class="card-body">
                             <div class="col-md-12">
                                 <button type="SUBMIT" class="btn btn-primary">Scan RFID</button>
                             </div>
                         </div>
-          </div>  
+          </div>
+            
         </div>   
     </div>
 </div>
