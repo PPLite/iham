878e69be48d96e8f0ef5e07efe35bd584eae142d
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-11 03:20:00 +0000

    tes

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -13,55 +13,23 @@ include('database/dbconfig.php');
                 <div class="card-header text-center">
                     <h4> Registrasi Asset Barang Baru</h4>
                 </div>
-                            <div class="card-body">
-                                <form action="" method="GET">
-                                <div class="row">
-                                    <div class="">
-                                        <input type="hidden" name="rfid_uid" value="<?php if(isset($_GET['rfid_uid'])) {echo $_GET['rfid_uid'];} ?>" class="form-control">
-                                    </div>
-                                <div class="col-md-4">
-                                    <button type="SUBMIT" class="btn btn-primary">Scan RFID</button>
-                                </div>
+                        <div class="card-body">
+                            <div class="col-md-4">
+                                <button type="SUBMIT" class="btn btn-primary">Scan RFID</button>
                             </div>
+                        </div>
 
                         <div class="row">
                             <div class="col-md-12">
-                                <hr>
-                                <?php 
-                                if(isset($_GET['rfid_uid']))
-                                    {
-                                        $query = "SELECT * FROM `tb_reader_scan` order by id desc limit 1";
-                                        $query_run = mysqli_query($connection, $query);
-                                        
 
-                                    if(mysqli_num_rows($query_run) > 0)
-                                    foreach($query_run as $row)
-                                    ?>
-                                    <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label for="">Kode UID</label>
-                                                <input type="text" value="<?= $row["rfid_uid"]; ?>" class="form-control" disabled>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="">Waktu Di Scan</label>
-                                                <input type="text" value="<?= $row["timestamp"]; ?>" class="form-control" disabled>
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="">Di Scan oleh Reader</label>
-                                                <input type="text" value="<?= $row["reader_id"]; ?>" class="form-control" disabled>
-                                            </div>
-                                        </div>
-                                    <?php
-                                 }
-                                 else
-                                 $row["rfid_uid"] = "";                                  
-                            ?>        
-                            </div>
+                            hewes hewes
+
                             </div>
                         </div>
-                    </form>
                 </div>
             </div>
+
+            
         </div>
     </div>
 </div>
