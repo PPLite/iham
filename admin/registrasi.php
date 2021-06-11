f102f76530e5fd565256f93de77b3b34eb4cf49a
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-11 17:44:27 +0000

    uhuh

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -29,9 +29,10 @@ include('database/dbconfig.php');
 
 
                             <div class="col-lg-5 mb-4">
-                                <div class="card bg-primary text-white shadow">
+                                <div class="card bg-success text-white shadow">
                                     <div class="card-body">
                                     <a href="tambah-aset-barang.php" class="text-white">Registrasi Bayi</a>
+                                    <i class="fas fa-baby-carriage fa-2x text-gray-300 float-right"></i>
                                     </div>
                                 </div>
                             </div>
