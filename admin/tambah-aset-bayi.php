4c41e105b775a56077e34b565375ebdd8214819d
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-11 18:08:00 +0000

    asdasdsad

--- admin/tambah-aset-bayi.php
+++ admin/tambah-aset-bayi.php
@@ -10,10 +10,34 @@ include('database/dbconfig.php');
     <div class="row justify-content-center">
         <div class="col-md-7">
 
-            <div class="card mt-5">
+          <div class="card mt-5">
+
                 <div class="card-header text-center">
-                    <h4> Registrasi Asset Bayi Baru</h4>
+                    <h4> Registrasi Asset Barang Baru</h4>
                 </div>
+
+                <div class="card-body">
+                    <div class="row justify-content-center">
+                
+                            <div class="col-lg-5 mb-4">
+                                <div class="card bg-primary text-white shadow">
+                                    <div class="card-body">
+                                    <a href="tambah-aset-barang.php" class="text-white">Registrasi Barang </a>
+                                    <i class="fas fa-archive fa-2x text-gray-300 float-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-5 mb-4">
+                                <div class="card bg-success text-white shadow">
+                                    <div class="card-body">
+                                    <a href="tambah-aset-bayi.php" class="text-white">Registrasi Bayi</a>
+                                    <i class="fas fa-baby-carriage fa-2x text-gray-300 float-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            
                             <div class="card-body">
                                 <form action="" method="GET">
                                 <div class="row">
