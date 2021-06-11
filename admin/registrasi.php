8a5ddb3bca52806de40cf28f4f15422e0ecf9187
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-11 14:13:00 +0000

    tes

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -35,6 +35,48 @@ include('database/dbconfig.php');
 
 
 
+<div class="row">
+          <div class="col-lg-4 mb-4">
+            <div class="card shadow mb-4">
+              <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Registrasi</h6>
+              </div>
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-lg-5 mb-4">
+                      <div class="card bg-primary text-white shadow">
+                        <div class="card-body" href="tambah-aset-barang.php">
+                        Registrasi Barang
+                        <div class="float-right">
+                          <i class="fas fa-archive fa-2x text-gray-300"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                    <div class="col-lg-5 mb-4">
+                    <div class="card bg-success text-white shadow">
+                  <div class="card-body">
+                    Registrasi Bayi
+                  <div class="float-right">
+                <i class="fas fa-baby-carriage fa-2x text-gray-300"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!--------Library------>
 <?php
