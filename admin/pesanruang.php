f32047ee9154855d44bcb655220fd1d53f69d122
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-14 15:27:31 +0000

    zxczxc

--- admin/pesanruang.php
+++ admin/pesanruang.php
@@ -1,13 +1,38 @@
+<!--------Library------>
 <?php
-include('security.php');
+include('security.php'); 
 include('includes/header.php'); 
 include('includes/navbar.php');
-include_once('database/dbconfig.php'); 
+include('database/dbconfig.php');
+
+?>
+<!----------Fitur
+>  User Management system
+-------->
+
+<?php
+if ($_SESSION['usertype']=="Admin")
+{
+    include('halaman/register.php');
+}
+else if ($_SESSION['usertype']=="engginer") 
+{
+    include('halaman/register.php');
+}
+else
+{
+    ?>
+        <script type="text/javascript">
+            window.location.href = 'error.php';
+        </script>
+    <?php
+}
 ?>
 
-Uhui
- 
 
+
+
+<!--------Library------>
 <?php
 include('includes/scripts.php');
 include('includes/footer.php');
