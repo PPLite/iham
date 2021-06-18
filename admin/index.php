c1d07ec491e14564160a7d17d08e770456200123
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-18 09:12:34 +0000

    tes tulisan

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,9 @@ include_once('database/dbconfig.php');
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">IHAM Dashboard </h1>
+    <h3 class="h3 mb-0 text-gray-800">Rumah Sakit Ibu Dan Anak </h3>
+    <h4 class="h3 mb-0 text-gray-800">Jl. Arif Rahmad Hakim. No 39. Sukolilo Surabaya </h4>
+
     <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
         class="fas fa-download fa-sm text-white-50"></i> Unduh Laporan Hari Ini</a>
   </div>
