83be2b9faf08b4b72e571fe4a5c6f35d0d8118d9
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-18 09:21:49 +0000

    breakdance

--- admin/index.php
+++ admin/index.php
@@ -12,9 +12,9 @@ include_once('database/dbconfig.php');
 
   <!-- Page Heading -->
   <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">IHAM Dashboard </h1> <br>
-    <h5 class="h5 mb-0 text-gray-800" >Rumah Sakit Ibu Dan Anak </h5> <br>
-    <h6 class="h7 mb-0 text-gray-800">Jl. Arif Rahmad Hakim. No 39. Sukolilo Surabaya </h6>
+    <h1 class="h3 mb-0 text-gray-800">IHAM Dashboard </h1> 
+    <br><h5 class="h5 mb-0 text-gray-800" >Rumah Sakit Ibu Dan Anak </h5>
+    <br><h6 class="h7 mb-0 text-gray-800">Jl. Arif Rahmad Hakim. No 39. Sukolilo Surabaya </h6>
 
     <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
         class="fas fa-download fa-sm text-white-50"></i> Unduh Laporan Hari Ini</a>
