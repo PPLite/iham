f7f932adfb676be82934ce41b5eb90e7c693e81b
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-21 02:20:16 +0000

    buk fik?

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -76,9 +76,7 @@ include('database/dbconfig.php');
                                             </div>
                                         </div>
                                     <?php
-                                 }
-                                 else
-                                 $row["rfid_uid"] = "";                                  
+                                 }                                 
                             ?>        
                             </div>
                             </div>
