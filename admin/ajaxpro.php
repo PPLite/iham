cf1136acf46b34b30829e76b0275ce7df88a399e
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-23 17:11:25 +0000

    siji sijine mung biso

---
+++ admin/ajaxpro.php
@@ -0,0 +1,41 @@
+<?php
+
+    
+
+    $hostName = "localhost";
+
+    $username = "root";
+
+    $password = "root";
+
+    $dbname = "test";
+
+ 
+
+    $mysqli = new mysqli($hostName, $username, $password, $dbname);
+
+ 
+
+    $sql = "SELECT * FROM countries WHERE name LIKE '%".$_GET['name']."%'";
+
+ 
+
+    $result = $mysqli->query($sql);
+
+ 
+
+    $response = [];
+
+    while($row = mysqli_fetch_assoc($result)){
+
+       $response[] = array("id"=>$row['id'], "name"=>$row['name']);
+
+    }
+
+ 
+
+    echo json_encode($response);
+
+ 
+
+?>
