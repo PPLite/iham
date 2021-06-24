5e97219a42d97ac10584a5dd3249c0de0834dda5
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-24 02:56:50 +0000

    ajax3

--- admin/ajaxpro.php
+++ admin/ajaxpro.php
@@ -5,17 +5,14 @@
     $password = "semogacepatlulus2021";
     $dbname = "mjdr3247_adminpanel";
  
-    $mysqli = new mysqli($hostName, $username, $password, $dbname);
+    $mysqli = new mysqli($hostName, $username, $password, $dbname); 
+    $sql = "SELECT * FROM products WHERE name LIKE '%".$_GET['name']."%'"; 
+    $all_result = $mysqli->query($sql);
  
-    $sql = "SELECT * FROM countries WHERE name LIKE '%".$_GET['name']."%'";
- 
-    $result = $mysqli->query($sql);
- 
-    $response = [];
-    while($row = mysqli_fetch_assoc($result)){
-       $response[] = array("id"=>$row['id'], "name"=>$row['name']);
-    }
- 
-    echo json_encode($response);
+    $data_response = [];
+    while($row = mysqli_fetch_assoc($all_result)){
+       $data_response[] = array("id"=>$row['id'], "name"=>$row['name']);
+    } 
+    echo json_encode($data_response);
  
 ?>
--- admin/chosen1.php
+++ admin/chosen1.php
@@ -1,24 +1,23 @@
-<!--- Sumber : https://www.itsolutionstuff.com/post/php-jquery-chosen-ajax-autocomplete-exampleexample.html ---->
-
 <!DOCTYPE html>
 <html>
 <head>
-  <title>PHP - Jquery Chosen Ajax Autocomplete Example - ItSolutionStuff.com</title>
+  <title>jQuery Chosen autocomplete with PHP and AJAX - pakainfo.com</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+ 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 </head>
 <body>
- 
-<div class="container">
-  <div class="panel panel-default">
-    <div class="panel-heading">PHP - Jquery Chosen Ajax Autocomplete Example - ItSolutionStuff.com</div>
-    <div class="panel-body">
+<h1>jQuery Chosen autocomplete with PHP and AJAX</h1> 
+<div class="container pakainfo">
+  <div class="pakainfo panel panel-primary">
+    <div class="panel-heading">jQuery Chosen autocomplete with PHP and AJAX - pakainfo.com</div>
+    <div class="panel-body pakainfo">
       <form>
-        <select class="form-control select-box">
-          <option>Select Option</option>
+        <select class="form-control pakainfo select-box">
+          <option>Select Products</option>
         </select>
       </form>
     </div>
@@ -35,8 +34,8 @@
         dataType: "json",
         success: function( data ) {
           $('.select-box').empty();
-          response( $.map( data, function( item ) {
-            $('.select-box').append('<option value="'+item.id+'">' + item.name + '</option>');
+          response( $.map( data, function( product ) {
+            $('.select-box').append('<option value="'+product.id+'">' + product.name + '</option>');
           }));
           $(".select-box").trigger("chosen:updated");
         }
