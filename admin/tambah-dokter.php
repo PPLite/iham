5dfe20958f06c49dee35c88b0311d5706a8ddcb3
guruh123 <40474727+guruh123@users.noreply.github.com>
2021-06-25 12:40:45 +0700

    Add files via upload

---
+++ admin/tambah-dokter.php
@@ -0,0 +1,162 @@
+<?php
+include('security.php'); 
+include('includes/header.php'); 
+include('includes/navbar.php');
+include('database/dbconfig.php');
+
+?>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+
+          <div class="card mt-5">
+
+                <div class="card-header text-center">
+                    <h4> Registrasi Dokter</h4>
+                </div>
+
+                <div class="card-body">
+                    <div class="row justify-content-center">
+                
+                            <div class="col-lg-5 mb-3">
+                                <div class="card bg-primary text-white shadow">
+                                    <div class="card-body">
+                                    <a href="tambah-aset-barang.php" class="text-white">Peralatan Medis </a>
+                                    <i class="fas fa-archive fa-2x text-gray-300 float-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-lg-4 mb-3">
+                                <div class="card bg-success text-white shadow">
+                                    <div class="card-body">
+                                    <a href="tambah-aset-bayi.php" class="text-white">Pasien Bayi</a>
+                                    <i class="fas fa-baby-carriage fa-2x text-gray-300 float-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-3 mb-3">
+                                <div class="card bg-success text-white shadow">
+                                    <div class="card-body">
+                                    <a href="tambah-dokter.php" class="text-white">Dokter</a>
+                                    <i class="fas fa-user-md fa-2x text-gray-300 float-right"></i>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <form action="" method="GET">
+                                <div class="row">
+                                    <div class="">
+                                        <input type="hidden" name="rfid_uid" value="<?php if(isset($_GET['rfid_uid'])) {echo $_GET['rfid_uid'];} ?>" class="form-control">
+                                    </div>
+                                <div class="col-md-4">
+                                    <button type="SUBMIT" class="btn btn-primary">Scan RFID</button>
+                                </div>
+                            </div>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <hr>
+                                <?php 
+                                if(isset($_GET['rfid_uid']))
+                                    {
+                                        $query = "SELECT * FROM `tb_reader_scan` order by id desc limit 1";
+                                        $query_run = mysqli_query($connection, $query);
+                                        
+
+                                    if(mysqli_num_rows($query_run) > 0)
+                                    foreach($query_run as $row)
+                                    ?>
+                                    <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="">Kode UID</label>
+                                                <input type="text" value="<?= $row["rfid_uid"]; ?>" class="form-control" disabled>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Waktu Di Scan</label>
+                                                <input type="text" value="<?= $row["timestamp"]; ?>" class="form-control" disabled>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Di Scan oleh Reader</label>
+                                                <input type="text" value="<?= $row["reader_id"]; ?>" class="form-control" disabled>
+                                            </div>
+                                        </div>
+                                    <?php
+                                 }                               
+                            ?>        
+                            </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <div class="card mt-5">
+                    <div class="card-body">
+
+<form action="code.php" method="POST">
+
+<div class="modal-body">
+
+        <div class="form-group">
+        <input type="hidden" name="rfid_uid" class="form-control" value="<?= $row["rfid_uid"]; ?>" placeholder="Masukkan kode UID" required data-readonly>
+        </div>     
+
+    <div class="form-group">
+        <label>Nama Dokter</label>
+        <input type="text" name="nama_dokter" class="form-control" placeholder="Masukkan Nama Dokter" required >
+    </div>
+
+    <div class="form-group">
+        <label>ID Dokter</label>
+        <input type="text" name="id_dokter" class="form-control" placeholder="Masukkan ID Dokter" required>
+    </div>
+
+    <div class="form-group">
+        <label>Jenis Kelamin</label>
+        <input type="text" name="Jenis_kelamin_dokter" class="form-control" placeholder="Masukkan Jenis Kelamin" required>
+    </div>
+
+    <div class="form-group">
+        <label>Alamat</label>
+        <input type="text" name="alamat_dokter" class="form-control" placeholder="Masukkan Alamat" required>
+    </div>
+
+    <div class="form-group">
+        <label>Spesisalis</label>
+        <input type="text" name="spesialis" class="form-control" placeholder="Masukkan Spesialis" required>
+    </div>
+
+    <div class="form-group">
+        <input type="hidden" name="status" class="form-control" value="perawatan" required>
+    </div>
+
+</div>
+<div class="modal-footer">
+    <button type="submit" name="daftarasetanak_btn" class="btn btn-primary">Simpan</button>
+</div>
+</form>
+            </div>
+        </div>
+    </div>
+</div
+
+<?php
+
+
+
+include('includes/scripts.php');
+include('includes/footer.php');
+?>
