43c4773c81441e8813ef3054e0ea43957138967f
PPLite <bagus.atmaja15@mhs.ee.its.ac.id>
2021-06-26 08:32:12 +0000

    perbaikan

--- admin/registrasi.php
+++ admin/registrasi.php
@@ -26,44 +26,19 @@ include('database/dbconfig.php');
                                 </div>
                             </div>
 
-                            <div class="col-lg-2 mb-3">
+                            <div class="col-lg-3 mb-3">
                                 <div class="card bg-success text-white shadow">
                                     <div class="card-body">
-                                    <a class="dropbtn">Pasien</a>
+                                    <a href="tambah-pasien.php" class="text-white">Pasien &nbsp;&nbsp;&nbsp;</a>
                                     <i class="fas fa-procedures fa-2x text-gray-300 float-right"></i>
                                     </div>
                                 </div>
                             </div>
 
-                            <script>
-                                /* When the user clicks on the button, 
-                                toggle between hiding and showing the dropdown content */
-                                function myFunction() {
-                                document.getElementById("myDropdown").classList.toggle("show");
-                                }
-
-                                // Close the dropdown if the user clicks outside of it
-                                window.onclick = function(event) {
-                                if (!event.target.matches('.dropbtn')) {
-                                    var dropdowns = document.getElementsByClassName("dropdown-content");
-                                    var i;
-                                    for (i = 0; i < dropdowns.length; i++) {
-                                    var openDropdown = dropdowns[i];
-                                    if (openDropdown.classList.contains('show')) {
-                                        openDropdown.classList.remove('show');
-                                    }
-                                    }
-                                }
-                                }
-                            </script>
-
-
-
-
                             <div class="col-lg-2 mb-3">
                                 <div class="card bg-success text-white shadow">
                                     <div class="card-body">
-                                    <a href="tambah-dokter.php" class="text-white">Dokter</a>
+                                    <a href="tambah-dokter.php" class="text-white">Dokter &nbsp;&nbsp;&nbsp;</a>
                                     <i class="fas fa-user-md fa-2x text-gray-300 float-right"></i>
                                     </div>
                                 </div>
@@ -139,50 +114,6 @@ include('database/dbconfig.php');
 
 
 
-
-
-
-<!-- tambahan ae -->
-<style>
-.dropbtn {
-  background-color: #3498DB;
-  color: white;
-  padding: 16px;
-  font-size: 16px;
-  border: none;
-  cursor: pointer;
-}
-
-.dropbtn:hover, .dropbtn:focus {
-  background-color: #2980B9;
-}
-
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f1f1f1;
-  min-width: 160px;
-  overflow: auto;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
-.dropdown a:hover {background-color: #ddd;}
-
-.show {display: block;}
-</style>
 
 <!--------Library------>
 <?php
--- admin/tambah-pasien.php
+++ admin/tambah-pasien.php
@@ -3,6 +3,7 @@ include('security.php');
 include('includes/header.php'); 
 include('includes/navbar.php');
 include('database/dbconfig.php');
+
 ?>
 
 <div class="container">
@@ -12,7 +13,7 @@ include('database/dbconfig.php');
           <div class="card mt-5">
 
                 <div class="card-header text-center">
-                    <h4> Registrasi Peralatan Medis</h4>
+                    <h4> Registrasi Pasien Baru</h4>
                 </div>
 
                 <div class="card-body">
@@ -44,6 +45,7 @@ include('database/dbconfig.php');
                                     </div>
                                 </div>
                             </div>
+
 
                             <div class="col-lg-2 mb-3">
                                 <div class="card bg-success text-white shadow">
@@ -84,31 +86,25 @@ include('database/dbconfig.php');
                                         $query_run = mysqli_query($connection, $query);
                                         
 
-                                            if(mysqli_num_rows($query_run) > 0)
-                                            foreach($query_run as $row)
-                                            {
-                                            ?>
-                                            <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="">Kode UID</label>
-                                                        <input type="text" value="<?= $row["rfid_uid"]; ?>" class="form-control" disabled>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="">Waktu Di Scan</label>
-                                                        <input type="text" value="<?= $row["timestamp"]; ?>" class="form-control" disabled>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <label for="">Di Scan oleh Reader</label>
-                                                        <input type="text" value="<?= $row["reader_id"]; ?>" class="form-control" disabled>
-                                                    </div>
-                                                </div>
-                                            <?php
-                                            }
-                                            else
-                                            {
-                                                $row["rfid_uid"]="";
-                                            }
-                                    }          
+                                    if(mysqli_num_rows($query_run) > 0)
+                                    foreach($query_run as $row)
+                                    ?>
+                                    <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="">Kode UID</label>
+                                                <input type="text" value="<?= $row["rfid_uid"]; ?>" class="form-control" disabled>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Waktu Di Scan</label>
+                                                <input type="text" value="<?= $row["timestamp"]; ?>" class="form-control" disabled>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Di Scan oleh Reader</label>
+                                                <input type="text" value="<?= $row["reader_id"]; ?>" class="form-control" disabled>
+                                            </div>
+                                        </div>
+                                    <?php
+                                 }                               
                             ?>        
                             </div>
                             </div>
@@ -121,47 +117,39 @@ include('database/dbconfig.php');
 </div>
 
 
-
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-7">
-            <div class="card mt-5">
-                    <div class="card-body">
+    <div class="col-md-18">
+        <div class="card mt-5">
 
-<form action="code.php" method="POST">
+                <div class="card-header text-center">
+                    <h4> Pilih Jenis Pasien</h4>
+                </div>
 
-<div class="modal-body">
+                <div class="card-body">
+                    <div class="row justify-content-center">
+                
+                        <div class="col-lg-3 mb-3">
+                                    <div class="card bg-primary text-white shadow">
+                                        <div class="card-body">
+                                        <a href="tambah-aset-barang.php" class="text-white">Pasien Dewasa</a>
+                                        <i class="fas fa-user-injured fa-2x text-gray-300 float-right"></i>
+                                        </div>
+                                    </div>
+                                </div>
 
-        <div class="form-group">
-        <input type="hidden" name="rfid_uid" class="form-control check_rfid_barang" value="<?= $row["rfid_uid"]; ?>" placeholder="Masukkan kode UID" required>
-        <small class="error_rfid" style="color:red;"></small>
-        </div>     
+                                <div class="col-lg-3 mb-3">
+                                    <div class="card bg-success text-white shadow">
+                                        <div class="card-body">
+                                        <a href="tambah-aset-bayi.php" class="text-white">Pasien Bayi</a>
+                                        <i class="fas fa-baby-carriage fa-2x text-gray-300 float-right"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-        <div class="form-group">
-                <label> Nama Aset </label>
-                <input type="text" name="nama_alat" class="form-control" placeholder="Masukkan Nama Asset" required>
-            </div>
-
-            <div class="form-group">
-                <label>Deskripsi</label>
-                <input type="text" name="deskripsi" class="form-control" placeholder="Masukkan deskripsi alat" required>
-            </div>
-
-            <div class="form-group">
-                <label>Penanggung Jawab</label>
-                <input type="text" name="penanggung_jawab" class="form-control" placeholder="Petugas yang bertanggungjawab" required>
-            </div>
- 
-            <div class="form-group">
-                <input type="hidden" name="status_asset" id="status_asset" value="tersedia" class="form-control">
-            </div>
-  
-</div>
-<div class="modal-footer">
-    <button type="submit" name="daftaraset_btn" class="btn btn-primary">Simpan</button>
-</div>
-</form>
-            </div>
+                    </div>
+                </div>
         </div>
     </div>
 </div>
