17c1b2b2037284009f6ff490b979f7ba9ae7fbb2
guruh123 <40474727+guruh123@users.noreply.github.com>
2021-07-11 14:07:48 +0700

    Add files via upload

--- admin/log-data-bayi.php
+++ admin/log-data-bayi.php
@@ -27,17 +27,9 @@ include('database/dbconfig.php');
         <table class="table table-bordered" id="tabelrfidscan2" width="100%" cellspacing="0">
           <thead>
             <tr>
-              <th>Timestamp</th>
               <th>RFID UID</th>
-              <th>ID Pengenal</th>
               <th>Nama Anak</th>
-              <th>Nama Ibu</th>
-              <th>Penanggung Jawab Bayi</th>
-              <th>Alamat</th>
-              </th>
-              <th>Status</th>
-              <th>Jumlah Terbaca</th>
-
+      
             </tr>
           </thead>
           <tbody>
@@ -49,15 +41,9 @@ include('database/dbconfig.php');
             ?>
                 <tr>
                   <!---Mengambil data dari database kemudian menampilkan ke tabel, serta menentukan kolom mana saja yang akan diambil datanya-->
-                  <td><?php echo $row['timestamp']; ?></td>
                   <td><?php echo $row['rfid_uid']; ?></td>
-                  <td><?php echo $row['id_pengenal']; ?></td>
                   <td><?php echo $row['nama_anak']; ?></td>
-                  <td><?php echo $row['nama_ibu']; ?></td>
-                  <td><?php echo $row['penanggung_jawab_bayi']; ?></td>
-                  <td><?php echo $row['alamat']; ?></td>
-                  <td><?php echo $row['status']; ?></td>
-                  <td><?php echo $row['jumlah']; ?>x</td>
+                  
               <?php
               }
             }
@@ -96,27 +82,15 @@ include('database/dbconfig.php');
         <!---Buat ngambil data--->
         <?php
         //dari database, dipilih semua (bintang = semuanya) dari tabel "tb_rfid"
-        $query = "SELECT COUNT(rfid_uid) AS jumlah, rfid_uid, id_pengenal, nama_anak, nama_ibu, penanggung_jawab_bayi, alamat, status, timestamp
-              FROM tb_hasil2_pasienbayi
-              GROUP BY nama_anak
-              ORDER BY `timestamp` DESC 
-    ";
+        $query = "SELECT * FROM tb_hasil_pasienbayi";
         $query_run = mysqli_query($connection, $query);
         ?>
 
         <table class="table table-bordered" id="tabelrfidscan4" width="100%" cellspacing="0">
           <thead>
             <tr>
-              <th>Timestamp</th>
               <th>RFID UID</th>
-              <th>ID Pengenal</th>
               <th>Nama Anak</th>
-              <th>Nama Ibu</th>
-              <th>Penanggung Jawab Bayi</th>
-              <th>Alamat</th>
-              </th>
-              <th>Status</th>
-              <th>Jumlah Terbaca</th>
             </tr>
           </thead>
           <tbody>
@@ -128,19 +102,12 @@ include('database/dbconfig.php');
             ?>
                 <tr>
                   <!---Mengambil data dari database kemudian menampilkan ke tabel, serta menentukan kolom mana saja yang akan diambil datanya-->
-                  <td><?php echo $row['timestamp']; ?></td>
                   <td><?php echo $row['rfid_uid']; ?></td>
-                  <td><?php echo $row['id_pengenal']; ?></td>
-                  <td><?php echo $row['nama_anak']; ?></td>
-                  <td><?php echo $row['nama_ibu']; ?></td>
-                  <td><?php echo $row['penanggung_jawab_bayi']; ?></td>
-                  <td><?php echo $row['alamat']; ?></td>
-                  <td><?php echo $row['status']; ?></td>
-                  <td><?php echo $row['jumlah']; ?>x</td>
+                  <td><?php echo $row['nama_anak']; ?></td>  
               <?php
               }
             }
-            //Jika gagal ngambil data akan mengeluarkan peringatan
+            //Jika gagal ngambil data akan mengeluarkan peringatan/
             else {
               echo "Data tidak ditemukan";
             }
